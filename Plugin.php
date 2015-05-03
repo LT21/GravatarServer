cfccc0e01e6dfbdf4114b40bbba189169786bba6
LT21 <zy-hzc@163.com>
2015-05-03 22:19:55 +0800

    添加默认头像设置功能

--- Plugin.php
+++ Plugin.php
@@ -1,11 +1,11 @@
 <?php
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 /**
- * 提供多个可选择的服务器，用以替换Typecho所使用的Gravatar官方服务器（www.gravatar.com）。避免在Gravatar官方服务器被墙后，评论中用户头像无法加载。
+ * 提供多个可选择的Gravatar头像服务器，同时提供默认头像设置。
  * 
  * @package Gravatar Server
  * @author LT21
- * @version 1.0.0
+ * @version 1.1.0
  * @link http://lt21.me
  */
 class GravatarServer_Plugin implements Typecho_Plugin_Interface
@@ -41,7 +41,7 @@ class GravatarServer_Plugin implements Typecho_Plugin_Interface
      */
     public static function config(Typecho_Widget_Helper_Form $form)
     {
-        /** 分类名称 */
+        /** 服务器 **/
         $server = new Typecho_Widget_Helper_Form_Element_Radio( 'server',  array(
                 'http://cn.gravatar.com'        =>  'Gravatar CN （ http://cn.gravatar.com ）',
                 'http://0.gravatar.com'         =>  'Gravatar 0 （ http://0.gravatar.com ）',
@@ -50,8 +50,19 @@ class GravatarServer_Plugin implements Typecho_Plugin_Interface
                 'http://3.gravatar.com'         =>  'Gravatar 3 （ http://3.gravatar.com ）',
                 'https://secure.gravatar.com'   =>  'Gravatar Secure （ https://secure.gravatar.com ）',
                 'http://gravatar.duoshuo.com'   =>  '多说 Gravatar 镜像 （ http://gravatar.duoshuo.com ）'),
-            'http://cn.gravatar.com', _t('选择服务器'), _t('选择服务器替换原Gravatar头像服务器') );
+            'http://cn.gravatar.com', _t('选择服务器'), _t('替换Typecho使用的Gravatar头像服务器（ www.gravatar.com ）') );
         $form->addInput($server->multiMode());
+
+        /** 默认头像 **/
+        $default = new Typecho_Widget_Helper_Form_Element_Radio( 'default',  array(
+                'mm'            =>  '<img src=http://cn.gravatar.com/avatar/926f6ea036f9236ae1ceec566c2760ea?s=32&r=G&forcedefault=1&d=mm height="32" width="32" /> 神秘人物',
+                'blank'         =>  '<img src=http://cn.gravatar.com/avatar/926f6ea036f9236ae1ceec566c2760ea?s=32&r=G&forcedefault=1&d=blank height="32" width="32" /> 空白',
+                ''				=>  '<img src=http://cn.gravatar.com/avatar/926f6ea036f9236ae1ceec566c2760ea?s=32&r=G&forcedefault=1&d= height="32" width="32" /> Gravatar 标志',
+                'identicon'     =>  '<img src=http://cn.gravatar.com/avatar/926f6ea036f9236ae1ceec566c2760ea?s=32&r=G&forcedefault=1&d=identicon height="32" width="32" /> 抽象图形（自动生成）',
+                'wavatar'       =>  '<img src=http://cn.gravatar.com/avatar/926f6ea036f9236ae1ceec566c2760ea?s=32&r=G&forcedefault=1&d=wavatar height="32" width="32" /> Wavatar（自动生成）',
+                'monsterid'     =>  '<img src=http://cn.gravatar.com/avatar/926f6ea036f9236ae1ceec566c2760ea?s=32&r=G&forcedefault=1&d=monsterid height="32" width="32" /> 小怪物（自动生成）'),
+            'mm', _t('选择默认头像'), _t('当评论者没有设置Gravatar头像时默认显示该头像') );
+        $form->addInput($default->multiMode());
     }
     
     /**
@@ -71,6 +82,7 @@ class GravatarServer_Plugin implements Typecho_Plugin_Interface
      */
     public static function render($size, $rating, $default, $comments)
     {
+        $default = Typecho_Widget::widget('Widget_Options')->plugin('GravatarServer')->default;
         $url = self::gravatarUrl($comments->mail, $size, $rating, $default, $comments->request->isSecure());
         echo '<img class="avatar" src="' . $url . '" alt="' . $comments->author . '" width="' . $size . '" height="' . $size . '" />';
     }
